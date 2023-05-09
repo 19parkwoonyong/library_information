eb4f185603ab4689ece0d0353796df113711cd1e
19parkwoonyong <131746079+19parkwoonyong@users.noreply.github.com>
2023-05-09 17:58:46 +0900

    Update open_api4.js

--- open_api_real/open_api/open_api4.js
+++ open_api_real/open_api/open_api4.js
@@ -10,16 +10,15 @@ function fetchAirQualityData(libraryName) {
     var url = 'http://apis.data.go.kr/6430000/cbLibraryInfoService/getLibraryInfo'; // 실제 API 엔드포인트로 교체
     var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'FW5DPPM8SSGQ6Ps%2BnXQLxumu9OWwkVONlMBaIWyHQ61xjYxOJYuL0ymuS4qzqYS7YajX1Dkdnn%2FaWtznUwr6rA%3D%3D'; // 실제 서비스 키로 교체
     // 필요한 추가 쿼리 매개변수를 추가하세요.
-    queryParams += '&' + encodeURIComponent('currentPage') + '=' + encodeURIComponent('1'); /**/
-    queryParams += '&' + encodeURIComponent('perPage') + '=' + encodeURIComponent('10'); /**/
-    queryParams += '&' + encodeURIComponent('FCLTY') + '=' + encodeURIComponent(libraryName); /**/
+    queryParams += '&' + encodeURIComponent('currentPage') + '=' + encodeURIComponent('1'); 
+    queryParams += '&' + encodeURIComponent('perPage') + '=' + encodeURIComponent('10'); 
+    queryParams += '&' + encodeURIComponent('FCLTY') + '=' + encodeURIComponent(libraryName); 
     queryParams += '&' + encodeURIComponent('')
     xhr.open('GET', url + queryParams);
     xhr.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(this.responseText);
-        // response에서 미세먼지 및 초미세먼지 데이터 추출
-        //var time= response.body[0].OPNNG;//실제 시간 
+        // response에서 미세먼지 및 초미세먼지 데이터 추출 
         var id = response.body[0].FCLTY;
         var location = response.body[0].ADRES;
         var over_floor = response.body[0].FLOOR; // 실제 응답 속성으로 교체
